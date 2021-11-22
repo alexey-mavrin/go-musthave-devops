ed58a1b4c1ae15eac0841cbaac7af8cff9af93c0
Alexey Mavrin <nirvam@yandex.ru>
2021-11-22 22:56:08 +0300

    server: use framework

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
+	"sync"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type statType int
+
+var statistics struct {
+	mu       sync.Mutex
+	counters map[string]int64
+	gauges   map[string]float64
+}
 
 const (
 	statTypeGauge = iota
@@ -28,24 +36,12 @@ type statReq struct {
 	valueGauge   float64
 }
 
-func parseURL(url string) (statReq, error) {
+func parseReq(r *http.Request) (statReq, error) {
 	var stat statReq
-	parts := strings.Split(url, "/")
-
-	var op, typ, name, rawVal string
-
-	if len(parts) > 1 {
-		op = parts[1]
-	}
-	if len(parts) > 2 {
-		typ = parts[2]
-	}
-	if len(parts) > 3 {
-		name = parts[3]
-	}
-	if len(parts) > 4 {
-		rawVal = parts[4]
-	}
+	op := chi.URLParam(r, "op")
+	typ := chi.URLParam(r, "typ")
+	name := chi.URLParam(r, "name")
+	rawVal := chi.URLParam(r, "rawVal")
 
 	if op != "update" {
 		return stat, errWrongOp
@@ -79,14 +75,62 @@ func parseURL(url string) (statReq, error) {
 		stat.valueGauge = val
 	}
 
+	stat.name = name
+
 	return stat, nil
 }
 
-// Handler — обработчик запроса.
-func Handler(w http.ResponseWriter, r *http.Request) {
+// Handler400 — return 400
+func Handler400(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte("OK"))
+}
+
+// MetricHandler prints all available metrics
+func MetricHandler(w http.ResponseWriter, r *http.Request) {
+	typ := chi.URLParam(r, "typ")
+	name := chi.URLParam(r, "name")
+	fmt.Println("GET", typ, name)
+
+	statistics.mu.Lock()
+	defer statistics.mu.Unlock()
+
+	if typ == "counter" {
+		val, ok := statistics.counters[name]
+		if !ok {
+			w.WriteHeader(http.StatusNotFound)
+		}
+		w.Write([]byte(fmt.Sprint(val)))
+	} else if typ == "gauge" {
+		val, ok := statistics.gauges[name]
+		if !ok {
+			w.WriteHeader(http.StatusNotFound)
+		}
+		w.Write([]byte(fmt.Sprint(val)))
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("OK"))
+	}
+}
+
+// DumpHandler prints all available metrics
+func DumpHandler(w http.ResponseWriter, r *http.Request) {
+	str := ""
+	statistics.mu.Lock()
+	for k, v := range statistics.counters {
+		str = str + fmt.Sprintf("%s %v\n", k, v)
+	}
+	for k, v := range statistics.gauges {
+		str = str + fmt.Sprintf("%s %v\n", k, v)
+	}
+	statistics.mu.Unlock()
+	w.Write([]byte(str))
+}
+
+// UpdateHandler — обработчик запроса.
+func UpdateHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Method, r.URL)
-	url := string(r.URL.Path)
-	_, err := parseURL(url)
+	stat, err := parseReq(r)
 
 	if err == errWrongOp {
 		w.WriteHeader(http.StatusNotFound)
@@ -108,11 +152,32 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	statistics.mu.Lock()
+	if stat.statType == statTypeCounter {
+		statistics.counters[stat.name] = stat.valueCounter
+	}
+	if stat.statType == statTypeGauge {
+		statistics.gauges[stat.name] = stat.valueGauge
+	}
+	statistics.mu.Unlock()
+
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
 
+// Router return chi.Router for testing and actual work
+func Router() chi.Router {
+	statistics.counters = make(map[string]int64)
+	statistics.gauges = make(map[string]float64)
+	r := chi.NewRouter()
+	r.Get("/", DumpHandler)
+	r.Get("/value/{typ}/{name}", MetricHandler)
+	r.Post("/{op}/{typ}/{name}/", Handler400)
+	r.Post("/{op}/{typ}/{name}/{rawVal}", UpdateHandler)
+	return r
+}
+
 func main() {
-	http.HandleFunc("/", Handler)
-	http.ListenAndServe(":8080", nil)
+	r := Router()
+	http.ListenAndServe(":8080", r)
 }
