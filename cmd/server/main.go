775f3d822acdc45f82d0e6bdfdfda6245d29077d
Alexey Mavrin <nirvam@yandex.ru>
2021-12-05 15:05:43 +0300

    server: parse flags

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,30 +1,87 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"log"
+	"os"
 	"time"
 
 	"github.com/alexey-mavrin/go-musthave-devops/internal/server"
 	"github.com/caarlos0/env/v6"
 )
 
+const (
+	defaultAddress   = "localhost:8080"
+	defaultInterval  = "300s"
+	defaultStoreFile = "/tmp/devops-metrics-db.json"
+	defaultRestore   = true
+)
+
 type config struct {
-	Address       string        `env:"ADDRESS" envDefault:"localhost:8080"`
-	StoreInterval time.Duration `env:"STORE_INTERVAL" envDefault:"300s"`
-	StoreFile     string        `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json"`
-	Restore       bool          `env:"RESTORE" envDefault:"true"`
+	Address       *string        `env:"ADDRESS"`
+	StoreInterval *time.Duration `env:"STORE_INTERVAL"`
+	StoreFile     *string        `env:"STORE_FILE"`
+	Restore       *bool          `env:"RESTORE"`
 }
 
-func main() {
+func setServerArgs() {
 	var cfg config
 	err := env.Parse(&cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
-	server.Config.Address = cfg.Address
-	server.Config.StoreInterval = cfg.StoreInterval
-	server.Config.StoreFile = cfg.StoreFile
-	server.Config.Restore = cfg.Restore
+
+	// distinguish between unset and set to ""
+	val, ok := os.LookupEnv("STORE_FILE")
+	if ok && val == "" {
+		empty := ""
+		cfg.StoreFile = &empty
+	}
+	log.Print(cfg)
+
+	addressFlag := flag.String("a", defaultAddress, "bind address")
+	intervalFlag := flag.String("i", defaultInterval, "store interval")
+	fileFlag := flag.String("f", defaultStoreFile, "store file")
+	restoreFlag := flag.Bool("r", defaultRestore, "restore")
+
+	flag.Parse()
+
+	if cfg.Address != nil {
+		server.Config.Address = *cfg.Address
+	} else {
+		server.Config.Address = *addressFlag
+	}
+
+	if cfg.StoreInterval != nil {
+		server.Config.StoreInterval = *cfg.StoreInterval
+	} else {
+		storeInterval, err := time.ParseDuration(*intervalFlag)
+		if err != nil {
+			log.Fatal("cant parse duration ", intervalFlag)
+		}
+		server.Config.StoreInterval = storeInterval
+	}
+
+	if cfg.StoreFile != nil {
+		server.Config.StoreFile = *cfg.StoreFile
+	} else {
+		server.Config.StoreFile = *fileFlag
+	}
+
+	if cfg.Restore != nil {
+		server.Config.Restore = *cfg.Restore
+	} else {
+		server.Config.Restore = *restoreFlag
+	}
+
+}
+
+func main() {
+	setServerArgs()
+
+	jsonConfig, _ := json.Marshal(server.Config)
+	log.Print("server started with ", string(jsonConfig))
 
 	server.StartServer()
 }
