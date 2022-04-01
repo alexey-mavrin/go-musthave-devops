82ff682e2065293b9653a15fce15d77acfb52446
Alexey Mavrin <nirvam@yandex.ru>
2022-04-01 14:23:07 +0300

    osexit: clean test

--- cmd/staticlint/internal/osexit/osexit_test.go
+++ cmd/staticlint/internal/osexit/osexit_test.go
@@ -2,7 +2,6 @@ package osexit_test
 
 import (
 	_ "embed"
-	"fmt"
 	"log"
 	"path"
 	"testing"
@@ -23,15 +22,11 @@ func Test(t *testing.T) {
 	files["main.go"] = mainFile
 	files["t/t.go"] = tFile
 
-	dir, _, err := analysistest.WriteFiles(files)
+	dir, clean, err := analysistest.WriteFiles(files)
 	if err != nil {
 		log.Fatal(err)
 	}
-	res := analysistest.Run(t, path.Join(dir, "src"), osexit.Analyzer)
+	analysistest.Run(t, path.Join(dir, "src"), osexit.Analyzer)
 
-	for r := range res {
-		fmt.Print(r)
-	}
-
-	// clean()
+	clean()
 }
